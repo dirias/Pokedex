8526643f8da10baf77062203264b804aabb66026
Didier Irias <didier@firstfactory.com>
2023-02-18 17:23:44 -0600

    Creating Stacks Navigations

--- src/navigation/Navigation.js
+++ src/navigation/Navigation.js
@@ -4,9 +4,9 @@ import { View, Text, TabBarIOSItem } from 'react-native'
 import { Image } from 'react-native';
 import { createMaterialBottomTabNavigator } from '@react-navigation/material-bottom-tabs';
 import Icon from 'react-native-vector-icons/FontAwesome5'
-import FavoriteScreen from '../screens/Favorite'
-import PokedexScreen from '../screens/Pokedex'
-import AccountScreen from '../screens/Account'
+import FavoriteNavigation from './FavoriteNavigation';
+import PokedexNavigation from './PokedexNavigation';
+import AccountNavigation from './AccountNavigation';
 
 //const Tab = createBottonTabNavigator();
 //Above just works with nav version5
@@ -15,19 +15,19 @@ const Tab = createMaterialBottomTabNavigator();
 export default function Navigation() {
   return (
     <Tab.Navigator>
-        <Tab.Screen name="Favorites" component={FavoriteScreen} options={
+        <Tab.Screen name="Favorites" component={FavoriteNavigation} options={
           {
             tabBarLabel:'Favoritos',
             tabBarIcon: ({color, size}) => <Icon name="heart" color={color} size={size}/>,
       }}
       />
-        <Tab.Screen name="Pokedex" component={PokedexScreen} options={
+        <Tab.Screen name="Pokedex" component={PokedexNavigation} options={
           {
             tabBarLabel:'',
             tabBarIcon: () => renderPokeball()
           }
         }/>
-        <Tab.Screen name="Account" component={AccountScreen} options={
+        <Tab.Screen name="Account" component={AccountNavigation} options={
           {
             tabBarLabel: 'Cuenta',
             tabBarIcon: ({color, size}) => <Icon name='user' color={color} size={size}/>}
